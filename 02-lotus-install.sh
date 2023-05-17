6082f33e34714c921965d3c60e3c689f7f4bb73c
anjor <anjor@umd.edu>
2023-05-17 15:12:03 +0200

    make install dir configurable

--- 02-lotus-install.sh
+++ 02-lotus-install.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/bash
 
-#Build and install Lotus
-cd /opt
-git clone https://github.com/filecoin-project/lotus.git
-git checkout releases
-export RUSTFLAGS="-C target-cpu=native -g"
-export FFI_BUILD_FROM_SOURCE=1
-export FFI_USE_MULTICORE_SDR=0
-make clean all
-sudo make install
+INSTALL_DIR="/opt"
+
+build_lotus() {
+  DIR=$1
+
+  cd $DIR
+  git clone https://github.com/filecoin-project/lotus.git
+  git checkout releases
+
+  export RUSTFLAGS="-C target-cpu=native -g"
+  export FFI_BUILD_FROM_SOURCE=1
+  export FFI_USE_MULTICORE_SDR=0
+  make clean all
+}
+
+install_lotus() {
+  sudo make install
+}
+
+build_lotus $INSTALL_DIR
+install_lotus
