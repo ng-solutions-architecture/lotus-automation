e8d38a0324ed50f7803b37b03a001aac6d686438
anjor <anjor@umd.edu>
2023-05-17 14:33:52 +0200

    functions

--- 01-lotus-prereqs.sh
+++ 01-lotus-prereqs.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/bash
 
-#Install software dependecies for Ubuntu
-sudo apt update
-sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev git-all wget -y && sudo apt upgrade -y
+install_software_deps() {
+  sudo apt update
+  sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev git-all wget -y && sudo apt upgrade -y
+}
 
-#Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env"
+install_rust() {
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source "$HOME/.cargo/env"
+}
 
-#Install Go
-wget -c https://golang.org/dl/go1.19.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-export PATH=$PATH:/usr/local/go/bin
-echo 'PATH' >> ~/.bashrc && source ~/.bashrc
+install_go() {
+  wget -c https://golang.org/dl/go1.19.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+  export PATH=$PATH:/usr/local/go/bin
+  echo 'PATH' >> ~/.bashrc && source ~/.bashrc
+}
+
+install_software_deps
+install_rust
+install_go
