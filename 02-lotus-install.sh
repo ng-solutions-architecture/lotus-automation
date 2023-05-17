de3801f8c50543029d8e8275e0407416571b6da5
Angelo Schalley <70083964+Angelo-gh3990@users.noreply.github.com>
2023-05-17 08:24:16 -0500

    Update 02-lotus-install.sh

--- 02-lotus-install.sh
+++ 02-lotus-install.sh
@@ -5,9 +5,9 @@ cd /opt
 git clone https://github.com/filecoin-project/lotus.git
 git checkout releases
 export RUSTFLAGS="-C target-cpu=native -g"
-echo 'export RUSTFLAGS="-C target-cpu=native -g"' >> /etc/profile
+echo 'export RUSTFLAGS="-C target-cpu=native -g"' >> $HOME/.bashrc
 export FFI_BUILD_FROM_SOURCE=1
-echo export FFI_BUILD_FROM_SOURCE=1 >> /etc/profile
+echo export FFI_BUILD_FROM_SOURCE=1 >> $HOME/.bashrc
 export FFI_USE_MULTICORE_SDR=0
 make clean all
 sudo make install
