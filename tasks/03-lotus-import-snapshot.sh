b4efa4aa96fa4cfeb0c1a9586de2da9fdfab725a
bobdubois <bob@bobdubois.be>
2023-05-19 18:16:35 +0200

    add timestamp for chain import

--- tasks/03-lotus-import-snapshot.sh
+++ tasks/03-lotus-import-snapshot.sh
@@ -19,10 +19,12 @@ import_snapshot() {
   LOG=$2
 
   echo "Importing chain snapshot. This takes some time (~30min) for mainnet."
+  echo "Current time is $(date +%T)"
+
   export LOTUS_PATH=$LOTUS_DIR
   echo "export LOTUS_PATH=$LOTUS_DIR" >> $HOME/.bashrc
   nohup lotus daemon --import-snapshot ${DIR}/latest-lotus-snapshot.zst >> ${LOG}/lotus.log 2>&1 &
-  
+
   if [ $USE_CALIBNET == "y" ];
   then sleep 10m
     else sleep 30m
