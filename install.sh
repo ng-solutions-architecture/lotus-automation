880fb568f868eb47b69df0cd2b1d135c8765ec2d
bobdubois <bob@bobdubois.be>
2023-05-19 14:03:20 +0200

    press key

--- install.sh
+++ install.sh
@@ -9,15 +9,19 @@ run_task() {
 
 echo "Installing lotus prerequisites..."
 run_task 01-lotus-prereqs.sh
+read -n 1 -s -r -p "Press any key to continue"
 
 echo "Installing lotus..."
 run_task 02-lotus-install.sh
+read -n 1 -s -r -p "Press any key to continue"
 
 echo "Importing lotus snapshot..."
 run_task 03-lotus-import-snapshot.sh
+read -n 1 -s -r -p "Press any key to continue"
 
 echo "Initializing lotus..."
 run_task 04-lotus-initialize.sh
+read -n 1 -s -r -p "Press any key to continue"
 
 echo "Initializing lotus miner..."
 run_task 05-lotus-miner-initialize.sh
