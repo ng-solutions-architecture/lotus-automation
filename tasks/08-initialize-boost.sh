4ead7e2b6cf4e258a423fd8a4226303b5ae0714c
bobdubois <bob@bobdubois.be>
2023-05-21 10:48:44 +0200

    init boost cont.

--- tasks/08-initialize-boost.sh
+++ tasks/08-initialize-boost.sh
@@ -22,6 +22,26 @@ run_boost() {
     boostd --vv run > ${DIR}/boost.log 2>&1 &
 }
 
+announce_boost() {
+  IP=$1
+  PORT=$2
+  PEERID=$(boostd net id)
+  
+  lotus-miner actor set-addrs /ip4/${IP}/tcp/${PORT}
+  lotus-miner actor set-peer-id ${PEERID}
+}
+
+set_boost_api() {
+  API_IP=$1
+  API_PORT=$2
+
+  TOKEN=$(boostd auth api-info --perm=admin)
+  export BOOST_API_INFO=${TOKEN}:/ip4/${API_IP}/tcp/${API_PORT/http}
+  echo "export BOOST_API_INFO=${TOKEN}:/ip4/${API_IP}/tcp/${API_PORT/http}" >> $HOME/.bashrc
+}
+
 set_extra_boost_vars
 initialize_boost
 run_boost ${LOG_DIR}
+announce_boost ${BOOST_PUB_IP} ${BOOST_P2P_PORT}
+set_boost_api ${BOOST_IP} ${BOOST_PORT}
