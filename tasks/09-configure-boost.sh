4d91fa0d7cff4ce246abcc5cacc0608210ed40c6
bobdubois <bob@bobdubois.be>
2023-05-22 14:09:11 +0200

    configure boost

--- tasks/09-configure-boost.sh
+++ tasks/09-configure-boost.sh
@@ -9,6 +9,11 @@ set_boost_config () {
     sed -i -e '/ListenAddresses =/ s/= .*/= \["\/ip4\/0.0.0.0\/tcp\/${BOOST_P2P_PORT}"\]/' ${BOOST_DIR}/config.toml
     sed -i -e '/AnnounceAddresses =/ s/= .*/= \["\/ip4\/${BOOST_PUB_IP}\/tcp\/${BOOST_P2P_PORT}"\]/' ${BOOST_DIR}/config.toml
     sed -i -e '/NoAnnounceAddresses =/ s/= .*/= \[\]/' ${BOOST_DIR}/config.toml
+
+    export ENV_BOOST_API_INFO=$(boostd auth api-info --perm=admin)
+    echo "export ENV_BOOST_API_INFO=$(boostd auth api-info --perm=admin)" >> $HOME/.bashrc
+    export BOOST_API_INFO=$(echo $ENV_BOOST_API_INFO | awk '{split($0,a,"="); print a[2]}')
+    echo "export BOOST_API_INFO=$(echo $ENV_BOOST_API_INFO | awk '{split($0,a,"="); print a[2]}')" >> $HOME/.bashrc
 }
 
 restart_boost () {
@@ -18,5 +23,9 @@ restart_boost () {
     boostd --vv run >> $1/boost.log 2>&1 &
 }
 
+start_booster_http() {}
+    booster-http run --api-boost=${BOOST_API_INFO} --api-fullnode=${FULLNODE_API_INFO} --api-storage=${MINER_API_INFO}
+
 set_boost_config
 restart_boost ${LOG_DIR}
+start_booster_http
