0eab62a4b62cab7e1bf71e5ed50d4275dedb7812
bobdubois <bob@bobdubois.be>
2023-05-22 18:17:19 +0200

    fix nginx config

--- tasks/10-configure-reverse-proxy.sh
+++ tasks/10-configure-reverse-proxy.sh
@@ -11,30 +11,30 @@ install_nginx () {
 configure_reverse_proxy () {
     sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/ipfs.key -out /etc/ssl/certs/ipfs.crt
 
-    sudo printf "
-# ipfs gateway config\n
-server \{\n
+    printf "
+\# ipfs gateway config\n
+server {\n
         listen 8443 ssl;\n
         listen [::]:8443 ssl;\n\n
 
         server_name ${BOOSTER_HTTP_DNS};\n\n
-        
+
         ssl_certificate /etc/ssl/certs/ipfs.crt;\n
         ssl_certificate_key /etc/ssl/private/ipfs.key;\n
         ssl_protocols TLSv1.2;\n
         ssl_prefer_server_ciphers on;\n
         ssl_ciphers ECDHE-RSA-AES256-GCM-SHA512:DHE-RSA-AES256-GCM-SHA512:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384;\n\n
 
-        location /ipfs/ \{\n
+        location /ipfs/ {\n
                 proxy_pass http://127.0.0.1:7777;\n
-        \}\n
-\}\n
-    " > /etc/nginx/sites-enabled/ipfs
+        }\n
+}\n
+    " | sudo tee /etc/nginx/sites-enabled/ipfs 2>&1 &
 }
 
 start_nginx () {
     sudo systemctl stop apache2
-    sudo sysmtectl disable apache2
+    sudo systemctl disable apache2
     sudo systemctl start nginx
     sudo systemctl enable nginx
 }
