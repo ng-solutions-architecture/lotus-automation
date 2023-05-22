42c0a88892155720a206744e9bb936478b47ecc1
bobdubois <bob@bobdubois.be>
2023-05-22 22:07:25 +0200

    Create 11-systemd-unit-files.sh

---
+++ tasks/11-systemd-unit-files.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+create_env_file () {
+    cat $HOME/.bashrc | grep export | awk '{split($0,a," "); print a[2]}' > $HOME/.lotus_env
+}
+
+install_systemd_daemon () {
+    printf "
+[Unit]\n
+Description=Lotus Daemon\n
+After=network-online.target\n
+Requires=network-online.target\n\n
+
+[Service]\n
+Environment=GOLOG_FILE=\"/var/log/lotus/lotus.log\"\n
+EnvironmentFile=$HOME/.lotus_env\n
+User=$(whoami)\n
+Group=$(groups | awk \'{split($0,a,\" \"); print a[1]}\')\n
+ExecStart=/usr/local/bin/lotus daemon\n
+Restart=always\n
+RestartSec=10\n\n
+
+MemoryAccounting=true\n
+MemoryHigh=8G\n
+MemoryMax=10G\n
+LimitNOFILE=8192:10240\n\n
+
+[Install]\n
+WantedBy=multi-user.target\n
+" | sudo tee /etc/systemd/lotus-daemon.service > /dev/null
+
+    sudo chmod 0644 /etc/systemd/lotus-daemon.service
+}
+
+install_systemd_miner () {
+    printf "
+[Unit]\n
+Description=Lotus Mïner\n
+After=network-online.target\n
+Requires=network-online.target\n\n
+
+[Service]\n
+Environment=GOLOG_FILE=\"/var/log/lotus/lotusminer.log\"\n
+EnvironmentFile=$HOME/.lotus_env\n
+User=$(whoami)\n
+Group=$(groups | awk \'{split($0,a,\" \"); print a[1]}\')\n
+ExecStart=/usr/local/bin/lotus-miner run\n\n
+
+[Install]\n
+WantedBy=multi-user.target\n
+" | sudo tee /etc/systemd/lotus-miner.service > /dev/null
+
+    sudo chmod 0644 /etc/systemd/lotus-miner.service
+}
+
+install_systemd_boostd () {
+    printf "
+[Unit]\n
+Description=Boostd\n
+After=network-online.target\n
+Requires=network-online.target\n\n
+
+[Service]\n
+Environment=GOLOG_FILE=\"/var/log/lotus/boostd.log\"\n
+EnvironmentFile=$HOME/.lotus_env\n
+User=$(whoami)\n
+Group=$(groups | awk \'{split($0,a,\" \"); print a[1]}\')\n
+ExecStart=/usr/local/bin/boostd --vv run\n\n
+
+[Install]\n
+WantedBy=multi-user.target\n
+" | sudo tee /etc/systemd/boostd.service > /dev/null
+
+    sudo chmod 0644 /etc/systemd/boostd.service
+}
+
+install_systemd_booster () {
+    printf "
+[Unit]\n
+Description=Booster-HTTP\n
+After=network-online.target\n
+Requires=network-online.target\n\n
+
+[Service]\n
+Environment=GOLOG_FILE=\"/var/log/lotus/booster-http.log\"\n
+EnvironmentFile=$HOME/.lotus_env\n
+User=$(whoami)\n
+Group=$(groups | awk \'{split($0,a,\" \"); print a[1]}\')\n
+ExecStart=/usr/local/bin/booster run --api-boost=$BOOST_API_INFO --api-fullnode=$FULLNODE_API_INFO --api-storage=$MINER_API_INFO\n\n
+
+[Install]\n
+WantedBy=multi-user.target\n
+" | sudo tee /etc/systemd/booster-http.service > /dev/null
+
+    sudo chmod 0644 /etc/systemd/booster-http.service
+}
+
+reload_systemd () {
+    sudo systemctl daemon-reload
+}
+
+install_systemd_daemon
+install_systemd_miner
+install_systemd_boostd
+
+if [ ${USE_BOOSTER_HTTP} == "y" ]; then
+    install_systemd_booster
+fi
