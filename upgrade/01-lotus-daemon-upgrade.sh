41d7db2e1dea02063cc4ba1fd557ad4d63432592
bobdubois <bob@bobdubois.be>
2023-05-23 16:23:09 +0200

    lotus daemon check version

--- upgrade/01-lotus-daemon-upgrade.sh
+++ upgrade/01-lotus-daemon-upgrade.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 check_lotus_daemon_version () {
-  installed_version=$(lotus version | awk \'{print $2}\'  | cut -d"-" -f1 | grep -v lotus)
-  echo $installed_version
+  installed_version=$(lotus version | awk '{print $2}'  | cut -d"-" -f1 | grep -v lotus)
+  if [ ${installed_version} == ${LOTUS_VERSION} ]; then
+    echo "The desired lotus version is already installed."
+    else
+        echo "Lotus daemon will be upgraded to ${LOTUS_VERSION}"
+    fi
 }
 
 check_lotus_daemon_version
