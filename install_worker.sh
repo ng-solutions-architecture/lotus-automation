bc1d61e10aabea770b13725fc2ffcee7ec39dbd6
bobdubois <bob@bobdubois.be>
2023-05-23 14:48:52 +0200

    update install_worker tasks

--- install_worker.sh
+++ install_worker.sh
@@ -11,3 +11,10 @@ echo "Installing prerequisites..."
 run_task 01-prereqs.sh
 read -n 1 -s -r -p "Press any key to continue"
 
+echo "Installing Lotus..."
+run_task 02-lotus-install.sh
+read -n 1 -s -r -p "Press any key to continue"
+
+echo "Running lotus-worker..."
+run_task 03-run-lotus-worker.sh
+read -n 1 -s -r -p "Press any key to continue"
