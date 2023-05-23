5f4ab2dcc590744bb20db75d2af191e11c9d8025
bobdubois <bob@bobdubois.be>
2023-05-23 15:03:05 +0200

    vars

--- worker/03-run-lotus-worker.sh
+++ worker/03-run-lotus-worker.sh
@@ -4,7 +4,7 @@ source $HOME/.bashrc
 source ./variables
 
 start_lotus_worker () {
-    lotus-worker run "${worker_tasks[@]}" > ${LOG_DIR}/lotus-worker.log 2>&1 &
+    lotus-worker run "${worker_tasks[@]}" --name="${LOTUS_WORKER_NAME}" > ${LOG_DIR}/lotus-worker.log 2>&1 &
 }
 
 start_lotus_worker
