f7a6bb4055b54a4d3e618f84c21169784319db36
bobdubois <bob@bobdubois.be>
2023-07-20 10:13:15 +0200

    boost config fix

--- deploy/08-initialize-boost.sh
+++ deploy/08-initialize-boost.sh
@@ -21,6 +21,14 @@ initialize_boost() {
        --max-staging-deals-bytes=50000000000   
 }
 
+set_boost_config() {
+    sed -i -e "/ListenAddress =/ s/= .*/= \"\/ip4\/0.0.0.0\/tcp\/${BOOST_PORT}\/http\"/" ${BOOST_DIR}/config.toml
+    sed -i -e "/RemoteListenAddress =/ s/= .*/= \"\/ip4\/${BOOST_IP}\/tcp\/${BOOST_PORT}\/http\"/" ${BOOST_DIR}/config.toml
+    sed -i -e "/ListenAddresses =/ s/= .*/= \[\"\/ip4\/0.0.0.0\/tcp\/${BOOST_P2P_PORT}\"\]/" ${BOOST_DIR}/config.toml
+    sed -i -e "/AnnounceAddresses =/ s/= .*/= \[\"\/ip4\/${BOOST_PUB_IP}\/tcp\/${BOOST_P2P_PORT}\"\]/" ${BOOST_DIR}/config.toml
+    sed -i -e "/NoAnnounceAddresses =/ s/= .*/= \[\]/" ${BOOST_DIR}/config.toml
+}
+
 run_boost() {
     DIR=$1
     boostd --vv run > ${DIR}/boost.log 2>&1 &
@@ -33,10 +41,10 @@ set_boost_api () {
     echo "export BOOST_API_INFO=$(echo $ENV_BOOST_API_INFO | awk '{split($0,a,"="); print a[2]}')" >> $HOME/.bashrc
     echo "Boost API env is: $ENV_BOOST_API_INFO"
     echo "Boost API is:$BOOST_API_INFO"
-    read -n 1 -s -r -p "Press any key to continue
 }
 
 set_extra_boost_vars
 initialize_boost
+set_boost_config
 run_boost ${LOG_DIR}
 set_boost_api
--- deploy/09-configure-boost.sh
+++ deploy/09-configure-boost.sh
@@ -16,22 +16,6 @@ announce_boost() {
   lotus-miner actor set-peer-id ${PEERID}
 }
 
-set_boost_config() {
-    sed -i -e "/ListenAddress =/ s/= .*/= \"\/ip4\/0.0.0.0\/tcp\/${BOOST_PORT}\/http\"/" ${BOOST_DIR}/config.toml
-    sed -i -e "/RemoteListenAddress =/ s/= .*/= \"\/ip4\/${BOOST_IP}\/tcp\/${BOOST_PORT}\/http\"/" ${BOOST_DIR}/config.toml
-    sed -i -e "/ListenAddresses =/ s/= .*/= \[\"\/ip4\/0.0.0.0\/tcp\/${BOOST_P2P_PORT}\"\]/" ${BOOST_DIR}/config.toml
-    sed -i -e "/AnnounceAddresses =/ s/= .*/= \[\"\/ip4\/${BOOST_PUB_IP}\/tcp\/${BOOST_P2P_PORT}\"\]/" ${BOOST_DIR}/config.toml
-    sed -i -e "/NoAnnounceAddresses =/ s/= .*/= \[\]/" ${BOOST_DIR}/config.toml
-}
-
-restart_boost() {
-    DIR=$1
-
-    killall boostd
-    boostd --vv run >> ${DIR}/boost.log 2>&1 &
-    sleep 10
-}
-
 start_booster_http() {
     DIR=$1
 
@@ -41,7 +25,5 @@ start_booster_http() {
     fi
 }
 
-set_boost_config
-restart_boost ${LOG_DIR}
 announce_boost ${BOOST_PUB_IP} ${BOOST_P2P_PORT}
 start_booster_http ${LOG_DIR}
